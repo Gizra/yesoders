aafea717063d3bc8e3cff9ffc053bd8f9cd2f68a
Amitai Burstein <amitai@gizra.com>
2016-08-31 23:23:44 +0300

    Assert user's info

--- test/Handler/ProfileSpec.hs
+++ test/Handler/ProfileSpec.hs
@@ -14,3 +14,15 @@ spec = withApp $ do
 
         get ProfileR
         statusIs 200
+
+    it "asserts shows user's information" $ do
+        user <- createUser "bar"
+        authenticateAs user
+
+        get ProfileR
+        let (Entity _ u) = user
+        -- Member for
+        htmlAnyContain ".ui.segment div" "Members for"
+
+        -- User's GitHub name
+        htmlAnyContain "a.header" $ unpack . userIdent u
