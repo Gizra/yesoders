9a18bf8725829134a139602a1f3a942240424fa0
Amitai Burstein <amitai@gizra.com>
2016-08-31 23:04:05 +0300

    Add assertion for anon and auth users

--- test/Handler/ProfileSpec.hs
+++ test/Handler/ProfileSpec.hs
@@ -4,7 +4,11 @@ import TestImport
 
 spec :: Spec
 spec = withApp $ do
-    it "asserts access to my-account for authenticated user" $ do
+    it "asserts no access to my-account for anonymous users" $ do
+        get ProfileR
+        statusIs 403
+
+    it "asserts access to my-account for authenticated users" $ do
         user <- createUser "foo"
         authenticateAs user
 
