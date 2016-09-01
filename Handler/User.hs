dea66da79c36470230880104fd03ac543189701a
Amitai Burstein <amitai@gizra.com>
2016-09-02 01:03:26 +0300

    Add users stab

--- Handler/User.hs
+++ Handler/User.hs
@@ -3,4 +3,31 @@ module Handler.User where
 import Import
 
 getUserR :: Text -> Handler Html
-getUserR text = error "Not yet implemented: getUserR"
+getUserR ident = do
+    userEntity <- runDB . getBy404 $ UniqueUser ident
+
+    let (Entity userId user) = userEntity
+    currentTime <- liftIO getCurrentTime
+
+    let memberFor = humanReadableTimeDiff currentTime (userCreated user)
+
+    -- @todo: Use helper function.
+    csrf <- fmap reqToken getRequest
+    let csrf' = fromMaybe "" csrf
+
+
+    -- @todo: Prevent own selection.
+    let flaggedId = userId
+
+    mFlagging <- runDB . getBy $ UniqueFlagMentor userId flaggedId
+
+    let (action, flagMessage) =
+            case mFlagging of
+                Just _ -> (Unflag, "Unark as mentor" :: Text)
+                Nothing -> (Flag, "Mark as mentor" :: Text)
+
+    let token = getValidToken csrf flaggedId action
+
+    defaultLayout $ do
+        setTitle . toHtml $ userIdent user `mappend` "'s User page"
+        $(widgetFile "user")
