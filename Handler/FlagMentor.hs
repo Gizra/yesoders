f6c5a6c39b08e632dcde60de49db1eadab7362f4
Amitai Burstein <amitai@gizra.com>
2016-09-03 01:18:25 +0300

    Add access callback on handler

--- Handler/FlagMentor.hs
+++ Handler/FlagMentor.hs
@@ -12,33 +12,39 @@ getFlagMentorR flaggedId action token = do
 
     token' <- getFlagTokenFromCsrf flaggedId action
 
-    if token' /= token
+    let uniqueEntity = UniqueFlagMentor authId flaggedId
+    if flagAccess uniqueEntity action
         then
-            -- defaultLayout $ toWidget [hamlet|token should be #{token'}|]
+            if token' /= token
+                then
+                    -- defaultLayout $ toWidget [hamlet|token should be #{token'}|]
+                    notFound
+                else do
+                    -- Insert a new flagging or delete an existing one, baesd on the
+                    -- action.
+                    _ <- case action of
+                            Unflag -> do
+                                runDB $ deleteBy uniqueEntity
+
+                            Flag -> do
+                                currentTime <- liftIO getCurrentTime
+                                _ <- runDB $ insertUnique FlagMentor
+                                    { flagMentorUser = authId
+                                    , flagMentorFlagging = flaggedId
+                                    , flagMentorCreated = currentTime
+                                    }
+                                return ()
+
+                    let nextAction = if action == Flag then Unflag else Flag
+                    nextToken <- getFlagTokenFromCsrf flaggedId nextAction
+
+                    urlRender <- getUrlRender
+
+                    -- Return JSON with the link to the next action.
+                    return $ object
+                        [ "action" .= nextAction
+                        -- , "data" .= urlRender $ FlagMentorR flaggedId nextAction nextToken
+                        ]
+        else
+            -- @todo: Change to access denied.
             notFound
-        else do
-            -- Insert a new flagging or delete an existing one, baesd on the
-            -- action.
-            _ <- case action of
-                    Unflag -> do
-                        runDB . deleteBy $ UniqueFlagMentor authId flaggedId
-
-                    Flag -> do
-                        currentTime <- liftIO getCurrentTime
-                        _ <- runDB $ insertUnique FlagMentor
-                            { flagMentorUser = authId
-                            , flagMentorFlagging = flaggedId
-                            , flagMentorCreated = currentTime
-                            }
-                        return ()
-
-            let nextAction = if action == Flag then Unflag else Flag
-            nextToken <- getFlagTokenFromCsrf flaggedId nextAction
-
-            urlRender <- getUrlRender
-
-            -- Return JSON with the link to the next action.
-            return $ object
-                [ "action" .= nextAction
-                -- , "data" .= urlRender $ FlagMentorR flaggedId nextAction nextToken
-                ]
