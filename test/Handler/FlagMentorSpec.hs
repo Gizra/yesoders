fef5d483d4d83e6be3ce4841ff42ca92b3976e5e
Amitai Burstein <amitai@gizra.com>
2016-09-04 01:18:53 +0300

    asserts token value is reproducible

--- test/Handler/FlagMentorSpec.hs
+++ test/Handler/FlagMentorSpec.hs
@@ -2,6 +2,9 @@ module Handler.FlagMentorSpec (spec) where
 
 import TestImport
 
+import Database.Persist.Sql (toSqlKey)
+import Utils.Flag
+
 spec :: Spec
 spec = withApp $ do
 
@@ -29,3 +32,11 @@ spec = withApp $ do
 
             get $ UserR (userIdent user)
             htmlCount ".flag-wrapper" 1
+
+        it "asserts token value is reproducible" $ do
+            let csrf = Just "1234" :: Maybe Text
+            let entityKey = toSqlKey 1 :: UserId
+            let token = getFlagToken csrf entityKey Flag
+
+            let expected = "58fbe9c10ce55a240b0b94b5bd619f18" :: Text
+            assertEq "Token is valid" expected token
