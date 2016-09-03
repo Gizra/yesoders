554e5dfaf2c729edfd3b9777d31e55770684139e
Amitai Burstein <amitai@gizra.com>
2016-09-03 20:55:37 +0300

    Statr adding tests

--- test/Handler/FlagMentorSpec.hs
+++ test/Handler/FlagMentorSpec.hs
@@ -6,5 +6,16 @@ spec :: Spec
 spec = withApp $ do
 
     describe "getFlagMentorR" $ do
-        error "Spec not implemented: getFlagMentorR"
+        it "asserts flag link doesn't show to own user" $ do
+            userEntity <- createUser "foo"
+            authenticateAs userEntity
 
+            get ProfileR
+            htmlCount ".flag-wrapper" 0
+
+        it "asserts flag link doesn't show to anonymous users" $ do
+            userEntity <- createUser "foo"
+            let (Entity _ user) = userEntity
+
+            get $ UserR (userIdent user)
+            htmlCount ".flag-wrapper" 1
