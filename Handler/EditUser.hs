6d513fb8966a7d559658161c5634c5f053c7d2a5
Amitai Burstein <amitai@gizra.com>
2016-10-03 22:44:19 +0300

    Add view/edit tabs

---
+++ Handler/EditUser.hs
@@ -0,0 +1,9 @@
+module Handler.EditUser where
+
+import Import
+
+getEditUserR :: Text -> Handler Html
+getEditUserR text = error "Not yet implemented: getEditUserR"
+
+postEditUserR :: Text -> Handler Html
+postEditUserR text = error "Not yet implemented: postEditUserR"
---
+++ test/Handler/EditUserSpec.hs
@@ -0,0 +1,14 @@
+module Handler.EditUserSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getEditUserR" $ do
+        error "Spec not implemented: getEditUserR"
+
+
+    describe "postEditUserR" $ do
+        error "Spec not implemented: postEditUserR"
+
